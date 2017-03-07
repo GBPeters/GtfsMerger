0193465d668a37060b6c35d8b852984cc590bddc
GBPeters <gijstebrij@gmail.com>
2017-03-07 11:34:53 +0100

    Renamed instal script

--- install.sh
+++ install.sh
@@ -7,13 +7,13 @@ echo "Installing PostgreSQL and PostGIS extension..."
 sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt xenial-pgdg main" >> /etc/apt/sources.list'
 wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
 sudo apt-get update
-sudo apt-get install postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
+sudo apt-get install -y postgresql-9.5-postgis-2.2 pgadmin3 postgresql-contrib-9.5 postgresql-9.5-postgis-2.2-scripts postgresql-server-dev-9.5 libpq-dev
 
 # Install python and other dependencies
 echo "Installing Python and dependencies..."
 sudo apt-get install python python-pip
-sudo pip install --upgrade pip
-pip install psycopg2
+sudo pip install -y --upgrade pip
+sudo pip install psycopg2
 
 # Create database
 echo "Configuring database..."
